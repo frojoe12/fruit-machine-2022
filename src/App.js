aa1a09bb74adf0940c56df235a9d516863123e8f
Joe Frost <joefrostdesign@gmail.com>
2022-09-03 12:25:41 +0100

    remove axios test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, {useEffect, useRef} from "react"
+import React from "react"
 import FruitMachine from "./components/FruitMachine"
-import { testPost } from './api'
 
 const style = { maxWidth:"600px", margin: "0 auto", backgroundColor:"#aaa" }
 const App = () => {
-  const effectRan = useRef(false)
-  useEffect(()=>{
-    if (effectRan.current === false) {
-      testPost();
-      return () => {
-        effectRan.current = true
-      }
-    }
-  },[])
+  
   return (
     <div style={style}>
-      <FruitMachine />
+    <FruitMachine />
     </div>
   );
 }
--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import { storeData } from '../util/http'
-
-export const testPost = () => {
-    storeData({name:"test",age:45})
-} 
